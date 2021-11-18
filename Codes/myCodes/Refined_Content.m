cd3ef0b01dec594f7ee57f62cd3a82701bf45647
ZhengdaoLI0602 <80500317+ZhengdaoLI0602@users.noreply.github.com>
2021-11-18 15:10:58 +0800

    Add files via upload

--- Codes/myCodes/Refined_Content.m
+++ Codes/myCodes/Refined_Content.m
@@ -1,9 +1,18 @@
 clear Refined_data
+%%
+% Co-drafted by Zhengdao LI, Pin Hsun LEE
+% Updated by Zhengdao LI on 2021.11.17
+%%
 
+% Refined_data_FinalV: contains all the feature values
+% Refined_data: contains both features and truth values before refinement
+
+whetherToPlot = 0;
+%%
 for idt = 1: size(GNSS_data,1)  %number of epoches
     Refined_data {idt,1} = GNSS_data{idt,1};   %index of epoch##
     Refined_data {idt,2} = length(GNSS_data{idt,3});  % number of SV ##
-    Refined_data {idt,3} = GNSS_data{idt,16}(3)^2; % EPV
+    Refined_data {idt,3} = sqrt(GNSS_data{idt,16}(3)^2); % EPV
     Refined_data {idt,4} = sqrt(GNSS_data{idt,16}(1)^2  +...
                                                     GNSS_data{idt,16}(2)^2); %EPH
 
@@ -36,92 +45,113 @@ for idt = 1: size(GNSS_data,1)  %number of epoches
 
 end
 Refined_data(idt2Delete,:)=[]; % To delete the invalid sample data
-
+%% Final Refinement
+Truth_value(:,1) = sqrt(cell2mat(Refined_data (:,3)).^2 + cell2mat(Refined_data (:,4)).^2 ); % truth value for positioing error
+Refined_data_FinalV = Refined_data;
+Refined_data_FinalV(:,3)=[];
+Refined_data_FinalV(:,3)=[];
 
 %% First Plot 
-figure;
-subplot(3,1,1)
-y0 = cell2mat(Refined_data (:,2));
-plot(y0,'b')
-xlim([0 1.2*size(Refined_data,1)])
-ylim([0 1.2*max(y0)])
-ylabel('nSat') 
-%%
-subplot(3,1,2)
-y1 = cell2mat(Refined_data (:,3));
-plot(y1,'b')
-xlim([0 1.2*size(Refined_data,1)])
-ylim([0 1.2*max(y1)])
-ylabel('EPV') 
-%%
-subplot(3,1,3)
-y2 = cell2mat(Refined_data (:,4));
-plot(y2,'b')
-xlim([0 1.2*size(Refined_data,1)])
-ylim([0 1.2*max(y2)])
-ylabel('EPH') 
-%% Second Plot
-figure; 
-subplot(3,1,1)
-y3 = cell2mat(Refined_data (:,5));
-plot(y3,'b')
-xlim([0 1.2*size(Refined_data,1)])
-ylim([0 1.2*max(y3)])
-ylabel('mean Elevation Angle') 
-%%
-subplot(3,1,2)
-y4 = cell2mat(Refined_data (:,6));
-plot(y4,'b')
-xlim([0 1.2*size(Refined_data,1)])
-ylim([0 1.2*max(y4)])
-ylabel('var Elevation Angle') 
-%%
-subplot(3,1,3)
-y5 = cell2mat(Refined_data (:,7));
-plot(y5,'b')
-xlim([0 1.2*size(Refined_data,1)])
-ylim([0 1.2*max(y5)])
-ylabel('C/N0') 
-%% Third Plot
-figure;
-subplot(3,1,1)
-y3 = cell2mat(Refined_data (:,8));
-plot(y3,'b')
-xlim([0 1.2*size(Refined_data,1)])
-ylim([0 1.2*max(y3)])
-ylabel('Pr residual RSS_e') 
-%%
-subplot(3,1,2)
-y4 = cell2mat(Refined_data (:,9));
-plot(y4,'b')
-xlim([0 1.2*size(Refined_data,1)])
-ylim([0 1.2*max(y4)])
-ylabel('mean Pr residual') 
-%%
-subplot(3,1,3)
-y5 = cell2mat(Refined_data (:,10));
-plot(y5,'b')
-xlim([0 1.2*size(Refined_data,1)])
-ylim([0 1.2*max(y5)])
-ylabel('var Pr residual') 
-%% Fourth Plot
-figure;
-subplot(3,1,1)
-y3 = cell2mat(Refined_data (:,11));
-plot(y3,'b')
-xlim([0 1.2*size(Refined_data,1)])
-ylim([0 1.2*max(y3)])
-ylabel('mean consistency checking') 
-%%
-subplot(3,1,2)
-y4 = cell2mat(Refined_data (:,12));
-plot(y4,'b')
-xlim([0 1.2*size(Refined_data,1)])
-ylim([0 1.2*max(y4)])
-ylabel('var consistency checking') 
+if whetherToPlot == 1
+    figure;
+    subplot(3,1,1)
+    y0 = cell2mat(Refined_data (:,2));
+    plot(y0,'b')
+    xlim([0 1.2*size(Refined_data,1)])
+    ylim([0 1.2*max(y0)])
+    ylabel('nSat') 
+    %%
+    subplot(3,1,2)
+    y1 = cell2mat(Refined_data (:,3));
+    plot(y1,'b')
+    xlim([0 1.2*size(Refined_data,1)])
+    ylim([0 1.2*max(y1)])
+    ylabel('EPV') 
+    %%
+    subplot(3,1,3)
+    y2 = cell2mat(Refined_data (:,4));
+    plot(y2,'b')
+    xlim([0 1.2*size(Refined_data,1)])
+    ylim([0 1.2*max(y2)])
+    ylabel('EPH') 
+    %% Second Plot
+    figure; 
+    subplot(3,1,1)
+    y3 = cell2mat(Refined_data (:,5));
+    plot(y3,'b')
+    xlim([0 1.2*size(Refined_data,1)])
+    ylim([0 1.2*max(y3)])
+    ylabel('mean Elevation Angle') 
+    %%
+    subplot(3,1,2)
+    y4 = cell2mat(Refined_data (:,6));
+    plot(y4,'b')
+    xlim([0 1.2*size(Refined_data,1)])
+    ylim([0 1.2*max(y4)])
+    ylabel('var Elevation Angle') 
+    %%
+    subplot(3,1,3)
+    y5 = cell2mat(Refined_data (:,7));
+    plot(y5,'b')
+    xlim([0 1.2*size(Refined_data,1)])
+    ylim([0 1.2*max(y5)])
+    ylabel('C/N0') 
+    %% Third Plot
+    figure;
+    subplot(3,1,1)
+    y3 = cell2mat(Refined_data (:,8));
+    plot(y3,'b')
+    xlim([0 1.2*size(Refined_data,1)])
+    ylim([0 1.2*max(y3)])
+    ylabel('Pr residual RSS_e') 
+    %%
+    subplot(3,1,2)
+    y4 = cell2mat(Refined_data (:,9));
+    plot(y4,'b')
+    xlim([0 1.2*size(Refined_data,1)])
+    ylim([0 1.2*max(y4)])
+    ylabel('mean Pr residual') 
+    %%
+    subplot(3,1,3)
+    y5 = cell2mat(Refined_data (:,10));
+    plot(y5,'b')
+    xlim([0 1.2*size(Refined_data,1)])
+    ylim([0 1.2*max(y5)])
+    ylabel('var Pr residual') 
+    %% Fourth Plot
+    figure;
+    subplot(3,1,1)
+    y3 = cell2mat(Refined_data (:,11));
+    plot(y3,'b')
+    xlim([0 1.2*size(Refined_data,1)])
+    ylim([0 1.2*max(y3)])
+    ylabel('mean consistency checking') 
+    %%
+    subplot(3,1,2)
+    y4 = cell2mat(Refined_data (:,12));
+    plot(y4,'b')
+    xlim([0 1.2*size(Refined_data,1)])
+    ylim([0 1.2*max(y4)])
+    ylabel('var consistency checking') 
+
+    %%
+    xlabel('Epoch') 
+end
 
 
 
+% subplot(5,1,2)
 
-%%
-xlabel('Epoch') 
+% subplot(5,1,3)
+% 
+% subplot(5,1,4)
+
+% subplot(5,1,5)
+
+
+%                     GNSS_data{idt,14}(idm,2),...%Pr_Error
+%                     GNSS_data{idt,16}(1),...%Error_east
+%                     GNSS_data{idt,16}(2),...%Error_north
+%                     GNSS_data{idt,16}(3),...%Error_up
+
+% GNSS_data{idt,3}(idm),...%PRN

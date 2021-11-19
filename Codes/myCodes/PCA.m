eb872611bb886239e8d79eb6bac8f4dee941ac2a
ZhengdaoLI0602 <80500317+ZhengdaoLI0602@users.noreply.github.com>
2021-11-19 11:10:48 +0800

    Add files via upload

--- Codes/myCodes/PCA.m
+++ Codes/myCodes/PCA.m
@@ -1,11 +1,11 @@
 %%
 % Co-drafted by Pin Hsun LEE, Zhengdao LI
-% Updated by Pin Hsun LEE on 2021.11.18
+% Updated by Zhengdao LI on 2021.11.17
 %%
 clear pca_data coeff score latent
 
 % Refined_data_FinalV = cell2mat(Refined_data_FinalV);
-pca_data = cell2mat(Refined_data(:,5:size(Refined_data,2)));
+pca_data = cell2mat(Refined_data_FinalV(:,2:size(Refined_data_FinalV,2)));
 
 % Normalize the data for PCA
 for i=1:size(pca_data,2)
@@ -13,12 +13,15 @@ for i=1:size(pca_data,2)
     PCA_std = std(pca_data(:,i));
     pca_data(:,i)=(pca_data(:,i) - PCA_ave)/PCA_std;
 end
+
+
+
 %% PCA
 [coeff,score,latent] = pca(pca_data, 'numComponent', 3);  
 
 %% Plot
-pos_error = cell2mat(Refined_data(:,4));
-p = scatter3(score(:,1),score(:,2),score(:,3),10,pos_error(:,1),'filled'); % PCA1; PCA2; PCA3
+% pos_error = cell2mat(Refined_data(:,4));
+p = scatter3(score(:,1),score(:,2),score(:,3),10,Truth_value(:,1),'filled'); % PCA1; PCA2; PCA3
 
 xlabel('PCA1');
 ylabel('PCA2');
@@ -28,10 +31,8 @@ c = colorbar;
 c.Label.String = 'Positioning Error (m)';
 axis([-2 4 -4 10 -8 6])
 
-%%
-rPCA1 = [min(score(:,1)), max(score(:,1))];
-rPCA2 = [min(score(:,2)), max(score(:,2))];
-rPCA3 = [min(score(:,3)), max(score(:,3))];
-
+% rPCA1 = [min(score(:,1)), max(score(:,1))];
+% rPCA2 = [min(score(:,2)), max(score(:,2))];
+% rPCA3 = [min(score(:,3)), max(score(:,3))];
 
 
--- Codes/myCodes/Refined_Content.m
+++ Codes/myCodes/Refined_Content.m
@@ -11,19 +11,19 @@ whetherToPlot = 0;
 %%
 for idt = 1: size(GNSS_data,1)  %number of epoches
     Refined_data {idt,1} = GNSS_data{idt,1};   %index of epoch##
-    Refined_data {idt,2} = sqrt(GNSS_data{idt,16}(3)^2); % EPV
-    Refined_data {idt,3} = sqrt(GNSS_data{idt,16}(1)^2  +...
-                                                    GNSS_data{idt,16}(2)^2); % EPH
+    Refined_data {idt,2} = length(GNSS_data{idt,3});  % number of SV ##
+    Refined_data {idt,3} = sqrt(GNSS_data{idt,16}(3)^2); % EPV
     Refined_data {idt,4} = sqrt(GNSS_data{idt,16}(1)^2  +...
-                                                    GNSS_data{idt,16}(2)^2 + GNSS_data{idt,16}(3)^2); % positioning error
-    Refined_data {idt,5} = length(GNSS_data{idt,3});  % number of SV ##
-    Refined_data {idt,6} = mean(GNSS_data{idt,4}); % mean elevation angle
-    Refined_data {idt,7} = var(GNSS_data{idt,4}); % variance elevation angle
+                                                    GNSS_data{idt,16}(2)^2); %EPH
+
+    Refined_data {idt,5} = mean(GNSS_data{idt,4}); % mean elevation angle
+    Refined_data {idt,6} = var(GNSS_data{idt,4}); % variance elevation angle
     
-    Refined_data {idt,8} = mean(GNSS_data{idt,9}(:,4),'all'); % C/N0
+    Refined_data {idt,7} = mean(GNSS_data{idt,9}(:,4)); % mean C/N0
+    Refined_data {idt,8} = mean(GNSS_data{idt,9}(:,4)); % var C/N0
     
-    Refined_data {idt,9} = mean(GNSS_data{idt,9}(:,9),'all'); % Pr residual RSS_e
-    Refined_data {idt,10} = mean(GNSS_data{idt,9}(:,6),'all'); % mean Pr residual
+    Refined_data {idt,9} = mean(GNSS_data{idt,9}(:,9)); % Pr residual RSS_e
+    Refined_data {idt,10} = mean(GNSS_data{idt,9}(:,6)); % mean Pr residual
     Refined_data {idt,11} = var(GNSS_data{idt,9}(:,6)); % variance Pr residual
     
     if size(Pr_rate{idt,1},2)~=0
@@ -38,7 +38,7 @@ end
 %% Further Process
 idt2Delete = [];
 for idt = 1: size(GNSS_data,1)  %number of epoches
-    if (size(Refined_data {idt,2},1)==0) ||(size(Refined_data {idt,3},1)==0)|| ...
+    if (size(Refined_data {idt,3},1)==0) ||(size(Refined_data {idt,4},1)==0)|| ...
         (Refined_data {idt,12} ==0) || (Refined_data {idt,12} ==9999)||....
         (Refined_data {idt,13} ==0)
         idt2Delete = [idt2Delete,idt];
@@ -47,89 +47,98 @@ for idt = 1: size(GNSS_data,1)  %number of epoches
 end
 Refined_data(idt2Delete,:)=[]; % To delete the invalid sample data
 %% Final Refinement
-%Truth_value(:,1) = sqrt(cell2mat(Refined_data (:,2)).^2 + cell2mat(Refined_data (:,3)).^2 ); % truth value for positioing error
-%Refined_data_FinalV = Refined_data;
-%Refined_data_FinalV(:,3)=[];
-%Refined_data_FinalV(:,3)=[];
+Truth_value(:,1) = sqrt(cell2mat(Refined_data (:,3)).^2 + cell2mat(Refined_data (:,4)).^2 ); % truth value for positioing error
 
-%% First Plot 
+Refined_data_FinalV = Refined_data;
+Refined_data_FinalV(:,3)=[];
+Refined_data_FinalV(:,3)=[];
+
+
+
 if whetherToPlot == 1
+    %% First Plot 
+    figure;
+    subplot(3,1,1)
+    y0 = cell2mat(Refined_data (:,2));
+    plot(y0,'b')
+    xlim([0 1.2*size(Refined_data,1)])
+    ylim([0 1.2*max(y0)])
+    ylabel('nSat') 
     %%
     subplot(3,1,2)
-    y1 = cell2mat(Refined_data (:,2));
+    y1 = cell2mat(Refined_data (:,3));
     plot(y1,'b')
     xlim([0 1.2*size(Refined_data,1)])
     ylim([0 1.2*max(y1)])
     ylabel('EPV') 
     %%
     subplot(3,1,3)
-    y2 = cell2mat(Refined_data (:,3));
+    y2 = cell2mat(Refined_data (:,4));
     plot(y2,'b')
     xlim([0 1.2*size(Refined_data,1)])
     ylim([0 1.2*max(y2)])
     ylabel('EPH') 
-    %%
-    figure;
-    subplot(3,1,1)
-    y0 = cell2mat(Refined_data (:,5));
-    plot(y0,'b')
-    xlim([0 1.2*size(Refined_data,1)])
-    ylim([0 1.2*max(y0)])
-    ylabel('nSat') 
     %% Second Plot
     figure; 
     subplot(3,1,1)
-    y3 = cell2mat(Refined_data (:,6));
+    y3 = cell2mat(Refined_data (:,5));
     plot(y3,'b')
     xlim([0 1.2*size(Refined_data,1)])
     ylim([0 1.2*max(y3)])
     ylabel('mean Elevation Angle') 
     %%
     subplot(3,1,2)
-    y4 = cell2mat(Refined_data (:,7));
+    y4 = cell2mat(Refined_data (:,6));
     plot(y4,'b')
     xlim([0 1.2*size(Refined_data,1)])
     ylim([0 1.2*max(y4)])
     ylabel('var Elevation Angle') 
     %%
     subplot(3,1,3)
-    y5 = cell2mat(Refined_data (:,8));
+    y5 = cell2mat(Refined_data (:,7));
     plot(y5,'b')
     xlim([0 1.2*size(Refined_data,1)])
     ylim([0 1.2*max(y5)])
-    ylabel('C/N0') 
+    ylabel('mean C/N0') 
     %% Third Plot
     figure;
     subplot(3,1,1)
-    y3 = cell2mat(Refined_data (:,9));
+    y3 = cell2mat(Refined_data (:,8));
     plot(y3,'b')
     xlim([0 1.2*size(Refined_data,1)])
     ylim([0 1.2*max(y3)])
-    ylabel('Pr residual RSS_e') 
+    ylabel('var C/N0') 
     %%
     subplot(3,1,2)
-    y4 = cell2mat(Refined_data (:,10));
+    y4 = cell2mat(Refined_data (:,9));
     plot(y4,'b')
     xlim([0 1.2*size(Refined_data,1)])
     ylim([0 1.2*max(y4)])
-    ylabel('mean Pr residual') 
+    ylabel('Pr residual RSS_e') 
     %%
     subplot(3,1,3)
-    y5 = cell2mat(Refined_data (:,11));
+    y5 = cell2mat(Refined_data (:,10));
     plot(y5,'b')
     xlim([0 1.2*size(Refined_data,1)])
     ylim([0 1.2*max(y5)])
-    ylabel('var Pr residual') 
+    ylabel('mean Pr residual') 
     %% Fourth Plot
     figure;
     subplot(3,1,1)
+    y3 = cell2mat(Refined_data (:,11));
+    plot(y3,'b')
+    xlim([0 1.2*size(Refined_data,1)])
+    ylim([0 1.2*max(y3)])
+    ylabel('var Pr residual') 
+    %%
+    subplot(3,1,2)
     y3 = cell2mat(Refined_data (:,12));
     plot(y3,'b')
     xlim([0 1.2*size(Refined_data,1)])
     ylim([0 1.2*max(y3)])
     ylabel('mean consistency checking') 
     %%
-    subplot(3,1,2)
+    subplot(3,1,3)
     y4 = cell2mat(Refined_data (:,13));
     plot(y4,'b')
     xlim([0 1.2*size(Refined_data,1)])
@@ -141,19 +150,3 @@ if whetherToPlot == 1
 end
 
 
-
-% subplot(5,1,2)
-
-% subplot(5,1,3)
-% 
-% subplot(5,1,4)
-
-% subplot(5,1,5)
-
-
-%                     GNSS_data{idt,14}(idm,2),...%Pr_Error
-%                     GNSS_data{idt,16}(1),...%Error_east
-%                     GNSS_data{idt,16}(2),...%Error_north
-%                     GNSS_data{idt,16}(3),...%Error_up
-
-% GNSS_data{idt,3}(idm),...%PRN
